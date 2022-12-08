d9c4b17375fde2dedfff1bbc76eeefc6eb7fadf7
Michiel van der Bruggen <michielvanderbruggen@gmail.com>
2022-12-08 22:17:16 +0100

    day8

---
+++ day8.py
@@ -0,0 +1,78 @@
+
+# 99x99
+forrest = []
+counted = []
+
+with open('input8.txt', 'r') as handler:
+    for line in handler:
+        forrest.append(line[:-1])
+
+# Add the outher rim
+for x in range(99):
+    counted.append(str(x) + ',0')
+for x in range(99):
+    counted.append(str(x) + ',98')
+for y in range(1, 98):
+    counted.append('0,' + str(y))
+for y in range(1, 98):
+    counted.append('98,' + str(y))
+
+# Left 2 right
+for y in range(1, 98):
+    hightree = int(forrest[y][0])
+    for x in range(1, 98):
+        treeloc = str(x) + ',' + str(y)
+        tree = int(forrest[y][x])
+        prevtree = int(forrest[y][x-1])
+        if tree > prevtree and tree > hightree and treeloc not in counted:
+            print('Tree', treeloc, 'has size',tree, 'and is bigger that', prevtree,'and highest', hightree)
+            counted.append(treeloc)
+            hightree = tree
+            print('Count:', len(counted))
+
+# Right 2 left
+for y in range(1, 99):
+    hightree = int(forrest[y][0])
+    for x in range(98, 0, -1):
+        treeloc = str(x) + ',' + str(y)
+        tree = int(forrest[y][x])
+        prevtree = int(forrest[y][x-1])
+        if tree > prevtree and tree > hightree and treeloc not in counted:
+            print('Tree', treeloc, 'has size',tree, 'and is bigger that', prevtree,'and highest', hightree)
+            counted.append(treeloc)
+            hightree = tree
+            print('Count:', len(counted))
+
+# Top 2 bottom
+for x in range(1, 99):
+    hightree = int(forrest[0][x])
+    for y in range(1, 98):
+        treeloc = str(x) + ',' + str(y)
+        tree = int(forrest[y][x])
+        prevtree = int(forrest[y][x-1])
+        if tree > prevtree and tree > hightree and treeloc not in counted:
+            print('Tree', treeloc, 'has size',tree, 'and is bigger that', prevtree,'and highest', hightree)
+            counted.append(treeloc)
+            hightree = tree
+            print('Count:', len(counted))
+
+# Bottom 2 top
+hightree = int(forrest[0][x])
+for x in range(1, 99):
+    for y in range(98, 0, -1):
+        treeloc = str(x) + ',' + str(y)
+        tree = int(forrest[y][x])
+        prevtree = int(forrest[y][x-1])
+        if tree > prevtree and tree > hightree and treeloc not in counted:
+            print('Tree', treeloc, 'has size',tree, 'and is bigger that', prevtree,'and highest', hightree)
+            counted.append(treeloc)
+            hightree = tree
+            print('Count:', len(counted))
+
+print()        
+print('Total score puzzle 1:', len(counted))
+# print('Total score puzzle 2:', total2)
+# 1943 too high
+# 1935 too high
+# 1538 too low
+# 1887 is false
--- test.py
+++ test.py
@@ -1,9 +1,26 @@
+forrest = []
+counted = []
 
-dir = '/nvvpbmgm/trjpcb/gsrrzgt/bwfnrbqdc/'
-print(dir)
-dir = dir[:dir.rfind('/', 0, -1)]
-print(dir)
+with open('input8.txt', 'r') as handler:
+    for line in handler:
+        forrest.append(line[:-1])
 
-dic = {}
-dic['key'] += 1
-print(dic)
+# Left 2 right
+y=1
+hightree = int(forrest[y][0])
+for x in range(1, 98):
+    treeloc = str(x) + ',' + str(y)
+    tree = int(forrest[y][x])
+    prevtree = int(forrest[y][x-1])
+    print()
+    print('treeloc=',treeloc)
+    print('tree=', tree)
+    print('prevtree=',prevtree)
+    if tree > prevtree and tree > hightree and treeloc not in counted:
+        print('Tree', treeloc, 'has size',tree, 'and is bigger that', prevtree,'and highest', hightree)
+        counted.append(treeloc)
+        hightree = tree
+        print('Count:', len(counted))
+
+# 301324204451515335252342253126503010214314026675523135330307170405066545120014006214521335013332134
+#   i
